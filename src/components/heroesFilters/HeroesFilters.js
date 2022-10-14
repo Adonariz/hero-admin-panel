f639b2593cea9cdb33a2a8e97fed2c5ff01a0027
alexey.zhila <aleksey.zhila@kartoteka.ru>
2022-10-14 17:51:33 +0300

    добавляет переключение класса активного фильтра

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 
 // Задача для этого компонента:
 // Фильтры должны формироваться на основании загруженных данных
@@ -7,20 +8,63 @@
 // Представьте, что вы попросили бэкенд-разработчика об этом
 
 const HeroesFilters = () => {
-    return (
-        <div className="card shadow-lg mt-4">
-            <div className="card-body">
-                <p className="card-text">Отфильтруйте героев по элементам</p>
-                <div className="btn-group">
-                    <button className="btn btn-outline-dark active">Все</button>
-                    <button className="btn btn-danger">Огонь</button>
-                    <button className="btn btn-primary">Вода</button>
-                    <button className="btn btn-success">Ветер</button>
-                    <button className="btn btn-secondary">Земля</button>
-                </div>
-            </div>
+  const itemRefs = useRef([]);
+
+  const onFilterClick = (evt) => {
+    const target = evt.target;
+
+    const items = itemRefs.current;
+    const targetIndex = items.indexOf(target);
+
+    items.forEach((item) => item.classList.remove("active"));
+
+    items[targetIndex].classList.add("active");
+  };
+
+  return (
+    <div className="card shadow-lg mt-4">
+      <div className="card-body">
+        <p className="card-text">Отфильтруйте героев по элементам</p>
+        <div className="btn-group">
+          <button
+            ref={(elem) => itemRefs.current.push(elem)}
+            onClick={onFilterClick}
+            className="btn btn-outline-dark active"
+          >
+            Все
+          </button>
+          <button
+            ref={(elem) => itemRefs.current.push(elem)}
+            className="btn btn-danger"
+            onClick={onFilterClick}
+          >
+            Огонь
+          </button>
+          <button
+            ref={(elem) => itemRefs.current.push(elem)}
+            className="btn btn-primary"
+            onClick={onFilterClick}
+          >
+            Вода
+          </button>
+          <button
+            ref={(elem) => itemRefs.current.push(elem)}
+            className="btn btn-success"
+            onClick={onFilterClick}
+          >
+            Ветер
+          </button>
+          <button
+            ref={(elem) => itemRefs.current.push(elem)}
+            className="btn btn-secondary"
+            onClick={onFilterClick}
+          >
+            Земля
+          </button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default HeroesFilters;
