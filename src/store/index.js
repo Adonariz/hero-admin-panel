013ae5d2c19cdf546da850f304c890110ba75474
alexey.zhila <aleksey.zhila@kartoteka.ru>
2022-10-26 17:43:14 +0300

    добавляет enhancer

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,39 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, compose } from "redux";
 import heroes from "../reducers/heroes";
 import filters from "../reducers/filters";
+
+const enhancer =
+  (createStore) =>
+  (...args) => {
+    const store = createStore(...args);
+
+    // создаем ссылку на оригинальный dispatch
+    const oldDispatch = store.dispatch;
+
+    // подменяем функцию dispatch
+    store.dispatch = (action) => {
+      // если в dispatch приходит строка, то объект создаем и возвращаем вручную
+      if (typeof action === "string") {
+        return oldDispatch({
+          type: action,
+        });
+      }
+
+      return oldDispatch(action);
+    };
+
+    return store;
+  };
 
 const store = createStore(
   combineReducers({
     heroes,
     filters,
   }),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(
+    enhancer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 export default store;
